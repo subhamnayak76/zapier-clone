d207df11f0a787f6e96a54e66527bf7837692bfe
subhamnayak76 <subhamnayak9638@gmail.com>
2024-12-10 23:01:22 +0530

    added signup page

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,25 +1,37 @@
 "use client"
 import { AppBar } from "@/components/Appbar";
 import { CheckFeature } from "@/components/CheckFeature";
+import { Input } from "@/components/Input";
+import { PrimaryButton } from "@/components/buttons/PrimaryButton";
 
 export default function() {
     return <div>
         <AppBar />
-        <div className="flex">
-         <div className="flex-1">
-            <div className="font-bold text-lg" >
+        <div className="flex justify-center">
+        <div className="flex pt-8 max-w-4xl ">
+         <div className="flex-1 pt-20 px-4">
+            <div className="font-semibold text-3xl px-4 pb-4" >
             Join millions worldwide who automate their work using Zapier.
             </div>
+            <div className="pb-4 pt-4">
             <CheckFeature label={"Easy setup, no coding required"} />
+            </div>
+            <div className="pb-4">
             <CheckFeature label = {"Free forever for core features"} />
+            </div>
             <CheckFeature label = {"14-day trial of premium features & apps"} />
             
         </div>
-        <div className='flex-1'>
-            <input type="text" placeholder="Your Name"></input>
-            <input type="text" placeholder="Your Email"></input>
-            <input type="password" placeholder="password"></input>
-
+        <div className='flex-1 pt-6 pb-6 px-4 border rounded '>
+            <Input label={"Name"} onChange={e =>{}} type="text" placeholder="Your name"/>
+            <Input label={"Email"} onChange={e =>{}} type="text" placeholder="Your email"/>
+            <Input label={"Password"} onChange={e =>{}} type="text" placeholder="Your password" />
+            <div className="pt-4">
+            <PrimaryButton onClick={() =>{
+                
+            }}size="big">Get started free</PrimaryButton>
+            </div>
+        </div>
         </div>
         </div>
     </div>
--- frontend/components/Input.tsx
+++ frontend/components/Input.tsx
@@ -1,10 +1,13 @@
 export const Input = ({label,placeholder,onChange,type="text"} : {
     label: string,
     placeholder : string,
-    onChange : () => void;
+    onChange : (e:any) => void;
     type? : 'text' | 'password'
 }) => {
     return <div>
-        <input type={type} placeholder={placeholder} onChange={onChange}></input>
+        <div className="text-sm pb-1 pt-2">
+            *<label>{label}</label>
+         </div>   
+        <input className="border rounded px-4 py-2 w-full border-black "type={type} placeholder={placeholder} onChange={onChange} />
     </div>
 }
